ee4c208c7369053ee0b8908dae8a8bb6cbad9e1c
Katchoou Lonzozou <lonzozoukatchoou@gmail.com>
2025-09-18 10:44:58 +0000

    Transformation of the data, plotting of all the necessary variables, with Microsoft stock example

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf  
 import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+from plotly.offline import iplot
+import plotly.graph_objs as go
+
+# os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  # Uncomment only if you need to disable oneDNN optimizations for troubleshooting
 
 
 def get_stock_data(ticker, start_date, end_date):
@@ -18,7 +21,19 @@ def get_stock_data(ticker, start_date, end_date):
     """
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
-    data = yf.download(ticker, start=start_date, end=end_date)
+    data = yf.Ticker(ticker)
+    new_data = data.history(start=start_date, end=end_date)
+    return new_data
+
+def transformed_data(data):
+    """
+    This function transforms the index of the DataFrame to datetime format
+    it also drops unnecessary columns
+    @param data: DataFrame containing stock data
+    @return: DataFrame with datetime index
+    """
+    data = data.drop(columns=['Dividends', 'Stock Splits'], axis=1)
+    data.index = pd.to_datetime(data.index)
     return data
 
 def plot_stock_data(data, title):
@@ -27,7 +42,8 @@ def plot_stock_data(data, title):
     @param data: DataFrame containing stock data
     @param title: The title prefered for the plot
     """
-    plt.figure(figsize=(14, 7))
+    data.plot(subplots=True, figsize=(12, 14))
+    plt.figure(figsize=(12, 8))
     plt.plot(data['Close'], label="Closing Price", color='blue', lw=1.5, linestyle="--")
     plt.plot(data['Open'], label="Opening Price", color='red', lw=1.3)
     plt.title(title)
@@ -36,45 +52,43 @@ def plot_stock_data(data, title):
     plt.legend()
     plt.show()
 
-def preprocessing(data):
+def normalise_data(data):
     """
-    This function preprocesses the stock data for model development
+    This function normalises the close price of the stock data for model development
     @param data: DataFrame containing stock data
-    @return: Scaled data and scaler object
+    @return: Scaled data object
     """
     scaler = MinMaxScaler(feature_range=(0, 1))
-    data_scaled = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
-    return data_scaled, scaler
+    scaled_data = data.copy()
+    scaled_data["Close"] = scaler.fit_transform(data[["Close"]])
+    scaled_data["High"] = scaler.fit_transform(data[["High"]])
+    scaled_data["Low"] = scaler.fit_transform(data[["Low"]])
+    scaled_data["Open"] = scaler.fit_transform(data[["Open"]])
+    scaled_data["Volume"] = scaler.fit_transform(data[["Volume"]])
+    return scaled_data
 
-def example_with_Microsoft_stock_price(ticker = 'MSFT'):
+def example_with_Microsoft_stock(ticker = 'MSFT'):
     """
     This function is an example of how to use the above functions with Microsoft stock data
     @param ticker: Stock ticker symbol (default is 'MSFT' for Microsoft)
     """
-        # Fetch historical stock data
-    data = get_stock_data(ticker, '2019-01-01', '2025-09-01')
-    #data.to_csv(f"{ticker}_data.csv", index=True)
+    data = get_stock_data(ticker, '2000-01-03', '2025-09-01')
     new_data = data.copy()
-    new_data.reset_index(inplace=True)
-    new_data.columns = ['Date', 'Close', 'High', 'Low', 'Open', 'Volume']
-    new_data['Date'] = pd.to_datetime(new_data['Date'])
-    new_data.set_index('Date', inplace=True)
-    print(new_data.head())
-    print(new_data.columns)
-
+    new_data = transformed_data(new_data)
     plot_stock_data(new_data, f"{ticker} Stock Price Data")
-    data_scaled, scaler = preprocessing(new_data)
-    
-    return new_data, data_scaled, scaler
+    data_scaled = normalise_data(new_data)
+    print(type(data_scaled))
+    print(data_scaled.head())
+    return data_scaled
 
 
 
 if __name__ == "__main__":
-    new_data, data_scaled, scaler = example_with_Microsoft_stock_price('MSFT')
-    new_data.describe()
-    print(f"The total number of null values per variable is:\n {new_data.isnull().sum()}")
-    print(f"The shape of the dataset is: {new_data.shape}")
-    print(f"The columns of the dataset are: {new_data.columns}")
+    data = example_with_Microsoft_stock('MSFT')
+    data.describe()
+    print(f"The total number of null values per variable is:\n {data.isnull().sum()}")
+    print(f"The shape of the dataset is: {data.shape}")
+    print(f"The columns of the dataset are: {data.columns}")
 
 
 
